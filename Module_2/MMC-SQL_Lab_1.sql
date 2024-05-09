8eaa985bf005e91b989c45adfaa4df3e7837423a
Khang <lenguyenkhang135@gmail.com>
2024-05-09 22:12:07 +0700

    Nop bai module2_buoi2

--- Module_2/MMC-SQL_Lab_1.sql
+++ Module_2/MMC-SQL_Lab_1.sql
@@ -1,66 +1,226 @@
 -- Active: 1714637042800@@127.0.0.1@3306@testing_system_db
+DROP DATABASE IF EXISTS Testing_System_Db;
+/*============================== CREATE TABLE=== =======================================*/
+/*======================================================================================*/
 CREATE DATABASE Testing_System_Db;
 USE Testing_System_Db;
+DROP TABLE IF EXISTS Deparment;
 CREATE TABLE Department (
-    DepartmentID                INT AUTO_INCREMENT PRIMARY KEY,
-    DepartmentName              VARCHAR(50)
+    DepartmentID                TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    DepartmentName              NVARCHAR(50)
 );
+DROP TABLE IF EXISTS `Position`;
 CREATE TABLE `Position` (
     PositionID                  INT AUTO_INCREMENT PRIMARY KEY,
-    PositionName               VARCHAR(50)
+    PositionName                ENUM('Dev','Test','Scrum Master','PM') NOT NULL
 );
+DROP TABLE IF EXISTS Account;
 CREATE TABLE Account (
-    AccountID                   INT AUTO_INCREMENT PRIMARY KEY,
+    AccountID                   INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Email                       VARCHAR(50),
     Username                    VARCHAR(50),
     Fullname                    VARCHAR(50),
-    DepartmentID                INT,
+    DepartmentID                TINYINT UNSIGNED,
     PositionID                  INT,
-    CreateDate                  DATE
+    CreateDate                  DATETIME DEFAULT NOW(),
+    CONSTRAINT fk_department_id
+    FOREIGN KEY (DepartmentID) 
+    REFERENCES Department (DepartmentID)
 );
+DROP TABLE IF EXISTS `Group`;
 CREATE TABLE `Group` (
-    GroupID                     INT AUTO_INCREMENT PRIMARY KEY,
+    GroupID                     INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     GroupName                   VARCHAR(50),
     CreatorID                   INT,
-    CreateDate                  DATE
+    CreateDate                  DATETIME DEFAULT NOW()
 );
+DROP TABLE IF EXISTS GroupAccount;
 CREATE TABLE GroupAccount (
-    GroupID                     INT,
-    AccountID                   INT,
+    GroupID                     INT UNSIGNED,
+    AccountID                   INT UNSIGNED,
     JoinDate                    DATE
 );
+ALTER TABLE GroupAccount ADD CONSTRAINT fk_Group_GroupID FOREIGN KEY (GroupID) REFERENCES `Group`(GroupID);
+ALTER TABLE GroupAccount ADD CONSTRAINT fk_Group_AccountID FOREIGN KEY (AccountID) REFERENCES Account(AccountID);
+DROP TABLE IF EXISTS TypeQuestion;
 CREATE TABLE TypeQuestion (
-    TypeID                      INT AUTO_INCREMENT PRIMARY KEY,
-    TypeName                    VARCHAR(50)
+    TypeID                      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TypeName                    ENUM('Essay','Multiple-Choice') NOT NULL
 );
+DROP TABLE IF EXISTS CatogoryQuestion;
 CREATE TABLE CategoryQuestion (
-    CategoryID                  INT AUTO_INCREMENT PRIMARY KEY,
+    CategoryID                  INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     CategoryName                VARCHAR(50)
 );
+DROP TABLE IF EXISTS Question;
 CREATE TABLE Question (
-    QuestionID                  INt AUTO_INCREMENT PRIMARY KEY,
+    QuestionID                  INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Content                     VARCHAR(500),
-    CategoryID                  INT,
-    TypeID                      INT,
+    CategoryID                  INT UNSIGNED,
+    TypeID                      INT UNSIGNED,
     CreatorID                   INT,
-    CreateDate                  DATE    
+    CreateDate                  DATETIME DEFAULT NOW(),
+    CONSTRAINT fk_category_id FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID),
+    CONSTRAINT fk_type_id FOREIGN KEY (TypeID) REFERENCES TypeQuestion (TypeID)
 );
+DROP TABLE IF EXISTS Answer;
 CREATE TABLE Answer (
-    AnswerID                    INT AUTO_INCREMENT PRIMARY KEY,
+    AnswerID                    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     Content                     VARCHAR(500),
-    QuestionID                  INT,
-    isCorrect                   VARCHAR(3)
+    QuestionID                  INT UNSIGNED,
+    isCorrect                   BIT DEFAULT 1,
+    CONSTRAINT fk_question_id FOREIGN KEY (QuestionID) REFERENCES Question (QuestionID)
 );
+DROP TABLE IF EXISTS Exam;
 CREATE TABLE Exam (
-    ExamID                      INT AUTO_INCREMENT PRIMARY KEY,
-    Code                        VARCHAR(6),
+    ExamID                      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Code                        CHAR(7),
     Title                       VARCHAR(50),
-    CategoryID                  INT,
+    CategoryID                  INT UNSIGNED,
     Duration                    INT,
     CreatorID                   INT,
-    CreateDate                  DATE
+    CreateDate                  DATETIME DEFAULT NOW(),
+    CONSTRAINT fk_category_id_exam FOREIGN KEY (CategoryID) REFERENCES CategoryQuestion (CategoryID)
 );
 CREATE TABLE ExamQuestion (
-    ExamID                      INT,
-    QuestionID                  INT
+    ExamID                      INT UNSIGNED,
+    QuestionID                  INT UNSIGNED
 );
+
+/*============================== INSERT DATABASE =======================================*/
+/*======================================================================================*/
+
+-- Add data Department
+INSERT INTO Department(DepartmentName) 
+VALUES
+						(N'Marketing'	),
+						(N'Sale'		),
+						(N'Bảo vệ'		),
+						(N'Nhân sự'		),
+						(N'Kỹ thuật'	),
+						(N'Tài chính'	),
+						(N'Phó giám đốc'),
+						(N'Giám đốc'	),
+						(N'Thư kí'		),
+						(N'Bán hàng'	);
+    
+-- Add data position
+INSERT INTO `Position`	(PositionName	) 
+VALUES 					
+                        ('Dev'			),
+						('Test'			),
+						('Scrum Master'	),
+						('PM'			); 
+
+
+-- Add data Account
+INSERT INTO `Account`(Email								, Username			, FullName				, DepartmentID	, PositionID, CreateDate)
+VALUES 				('haidang29productions@gmail.com'	, 'dangblack'		,'Nguyễn hải Đăng'		,   '5'			,   '1'		,'2020-03-05'),
+					('account1@gmail.com'				, 'quanganh'		,'Nguyen Chien Thang2'		,   '1'			,   '2'		,'2020-03-05'),
+                    ('account2@gmail.com'				, 'vanchien'		,'Nguyen Van Chien'		,   '2'			,   '3'		,'2020-03-07'),
+                    ('account3@gmail.com'				, 'cocoduongqua'	,'Duong Do'				,   '3'			,   '4'		,'2020-03-08'),
+                    ('account4@gmail.com'				, 'doccocaubai'		,'Nguyen Chien Thang1'	,   '4'			,   '4'		,'2020-03-10'),
+                    ('dapphatchetngay@gmail.com'		, 'khabanh'			,'Ngo Ba Kha'			,   '6'			,   '3'		,'2020-04-05'),
+                    ('songcodaoly@gmail.com'			, 'huanhoahong'		,'Bui Xuan Huan'		,   '7'			,   '2'		, NULL		),
+                    ('sontungmtp@gmail.com'				, 'tungnui'			,'Nguyen Thanh Tung'	,   '8'			,   '1'		,'2020-04-07'),
+                    ('duongghuu@gmail.com'				, 'duongghuu'		,'Duong Van Huu'		,   '9'			,   '2'		,'2020-04-07'),
+                    ('vtiaccademy@gmail.com'			, 'vtiaccademy'		,'Vi Ti Ai'				,   '10'		,   '1'		,'2020-04-09');
+
+-- Add data Group
+INSERT INTO `Group`	(  GroupName			, CreatorID		, CreateDate)
+VALUES 				(N'Testing System'		,   5			,'2019-03-05'),
+					(N'Development'			,   1			,'2020-03-07'),
+                    (N'VTI Sale 01'			,   2			,'2020-03-09'),
+                    (N'VTI Sale 02'			,   3			,'2020-03-10'),
+                    (N'VTI Sale 03'			,   4			,'2020-03-28'),
+                    (N'VTI Creator'			,   6			,'2020-04-06'),
+                    (N'VTI Marketing 01'	,   7			,'2020-04-07'),
+                    (N'Management'			,   8			,'2020-04-08'),
+                    (N'Chat with love'		,   9			,'2020-04-09'),
+                    (N'Vi Ti Ai'			,   10			,'2020-04-10');
+
+-- Add data GroupAccount
+INSERT INTO `GroupAccount`	(  GroupID	, AccountID	, JoinDate	 )
+VALUES 						(	1		,    1		,'2019-03-05'),
+							(	1		,    2		,'2020-03-07'),
+							(	3		,    3		,'2020-03-09'),
+							(	3		,    4		,'2020-03-10'),
+							(	5		,    5		,'2020-03-28'),
+							(	1		,    3		,'2020-04-06'),
+							(	1		,    7		,'2020-04-07'),
+							(	8		,    3		,'2020-04-08'),
+							(	1		,    9		,'2020-04-09'),
+							(	10		,    10		,'2020-04-10');
+
+
+-- Add data TypeQuestion
+INSERT INTO TypeQuestion	(TypeName			) 
+VALUES 						('Essay'			), 
+							('Multiple-Choice'	); 
+
+
+-- Add data CategoryQuestion
+INSERT INTO CategoryQuestion		(CategoryName	)
+VALUES 								('Java'			),
+									('ASP.NET'		),
+									('ADO.NET'		),
+									('SQL'			),
+									('Postman'		),
+									('Ruby'			),
+									('Python'		),
+									('C++'			),
+									('C Sharp'		),
+									('PHP'			);
+													
+-- Add data Question
+INSERT INTO Question	(Content			, CategoryID, TypeID		, CreatorID	, CreateDate )
+VALUES 					(N'Câu hỏi về Java Câu hỏi về Java Câu hỏi về Java Câu hỏi về Java'	,	1		,   '1'			,   '2'		,'2020-04-05'),
+						(N'Câu Hỏi về PHP'	,	10		,   '2'			,   '2'		,'2020-04-05'),
+						(N'Hỏi về C#'		,	9		,   '2'			,   '3'		,'2020-04-06'),
+						(N'Hỏi về Ruby'		,	6		,   '1'			,   '4'		,'2020-04-06'),
+						(N'Hỏi về Postman'	,	5		,   '1'			,   '5'		,'2020-04-06'),
+						(N'Hỏi về ADO.NET'	,	3		,   '2'			,   '6'		,'2020-04-06'),
+						(N'Hỏi về ASP.NET'	,	2		,   '1'			,   '7'		,'2020-04-06'),
+						(N'Hỏi về C++'		,	8		,   '1'			,   '8'		,'2020-04-07'),
+						(N'Hỏi về SQL'		,	4		,   '2'			,   '9'		,'2020-04-07'),
+						(N'Hỏi về Python'	,	7		,   '1'			,   '10'	,'2020-04-07');
+
+-- Add data Answers
+INSERT INTO Answer	(  Content		, QuestionID	, isCorrect	)
+VALUES 				(N'Trả lời 01'	,   1			,	0		),
+					(N'Trả lời 02'	,   1			,	1		),
+                    (N'Trả lời 03'	,   1			,	0		),
+                    (N'Trả lời 04'	,   1			,	1		),
+                    (N'Trả lời 05'	,   2			,	1		),
+                    (N'Trả lời 06'	,   3			,	1		),
+                    (N'Trả lời 07'	,   4			,	0		),
+                    (N'Trả lời 08'	,   8			,	0		),
+                    (N'Trả lời 09'	,   9			,	1		),
+                    (N'Trả lời 10'	,   10			,	1		);
+	
+-- Add data Exam
+INSERT INTO Exam	(`Code`			, Title					, CategoryID	, Duration	, CreatorID		, CreateDate )
+VALUES 				('VTIQ001'		, N'Đề thi C#'			,	1			,	60		,   '5'			,'2019-04-05'),
+					('VTIQ002'		, N'Đề thi PHP'			,	10			,	60		,   '2'			,'2019-04-05'),
+                    ('VTIQ003'		, N'Đề thi C++'			,	9			,	120		,   '2'			,'2019-04-07'),
+                    ('VTIQ004'		, N'Đề thi Java'		,	6			,	60		,   '3'			,'2020-04-08'),
+                    ('VTIQ005'		, N'Đề thi Ruby'		,	5			,	120		,   '4'			,'2020-04-10'),
+                    ('VTIQ006'		, N'Đề thi Postman'		,	3			,	60		,   '6'			,'2020-04-05'),
+                    ('VTIQ007'		, N'Đề thi SQL'			,	2			,	60		,   '7'			,'2020-04-05'),
+                    ('VTIQ008'		, N'Đề thi Python'		,	8			,	60		,   '8'			,'2020-04-07'),
+                    ('VTIQ009'		, N'Đề thi ADO.NET'		,	4			,	90		,   '9'			,'2020-04-07'),
+                    ('VTIQ010'		, N'Đề thi ASP.NET'		,	7			,	90		,   '10'		,'2020-04-08');
+                    
+                    
+-- Add data ExamQuestion
+INSERT INTO ExamQuestion(ExamID	, QuestionID	) 
+VALUES 					(	1	,		5		),
+						(	2	,		10		), 
+						(	3	,		4		), 
+						(	4	,		3		), 
+						(	5	,		7		), 
+						(	6	,		10		), 
+						(	7	,		2		), 
+						(	8	,		10		), 
+						(	9	,		9		), 
+						(	10	,		8		); 
